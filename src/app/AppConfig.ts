7fc0625e3e155be9c1f53bca9fcd6ec9083aabdd
“Edcodigo01” <edcodigo01@gmail.com>
2024-05-30 08:32:28 -0500

    first commit

---
+++ src/app/AppConfig.ts
@@ -0,0 +1,6 @@
+export class AppConfig {
+  public static ENDPOINT = 'https://maximarketb.factusis.com/';
+  // public static ENDPOINT = 'http://192.168.0.128:8000/public/api/';
+  // public static ENDPOINT = 'http://localhost:8000/api/';
+  //  public static ENDPOINT ='https://csempaquesb.actfacturas.com/public/api/';
+}
